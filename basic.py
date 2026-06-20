6e4fce308413e471a6c1482911846b6319043186
Gyanendrajeet <gyanendrashukla685@gmail.com>
2026-06-20 15:13:17 +0530

    added day2 half

---
+++ basic.py
@@ -0,0 +1,21 @@
+# variable 
+a=10 # integer variable 
+print(a) 
+print(type(a))
+
+a=10.5 # float variable 
+print(a)
+a="Hello" #string variable '' "" """"""
+a=true # boolean variable
+a=1=1j # complex variable 
+a=1,2,3,4 # tuple variable ()tuple
+a=[1,2,3,4] # list variable []list
+a= {1,2,3,4,} # set variable{}set
+a= {1: "one", 2: "two", 3: "three"} #dictionary
+
+class Myclass:
+    pass
+
+# 1_data = 100 # variable starting with number is not allowed 
+# _data=100 # variable starting with underscore is allowed 
+# @data=100 # variable starting with @ is not allowed 
